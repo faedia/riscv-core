7a8b9c266a71be2a1026b8fe142f81f3d51b7182
Steven Tomlinson <t97steven@gmail.com>
2021-05-29 20:48:17 +0100

    Added google test with first test that register x0 is always 0

--- test/common/MakeTest.mk
+++ test/common/MakeTest.mk
@@ -15,19 +15,23 @@ VERILATOR_MODULE_LIB = $(VERILATOR_GEN_DIR)/$(VERILATOR_PREFIX)__ALL.a
 
 gen_files: $(VERILATOR_MOULE_MK)
 
-$(VERILATOR_GEN_DIR):
-	mkdir -p $(VERILATOR_GEN_DIR)
-
 $(VERILATOR_MODULE_LIB): $(VERILATOR_MOULE_MK)
 	$(MAKE) -C $(VERILATOR_GEN_DIR) -f $(VERILATOR_PREFIX).mk
 
-$(VERILATOR_MOULE_MK): $(ROOT)/src/$(TEST_MODULE).sv $(VERILATOR_GEN_DIR)
+$(VERILATOR_MOULE_MK): $(ROOT)/src/$(TEST_MODULE).sv
+	mkdir -p $(VERILATOR_GEN_DIR)
 	verilator --Mdir $(VERILATOR_GEN_DIR) -Wall --threads 16 --trace -sv --cc -CFLAGS "-std=c++2a -fconcepts" $(ROOT)/src/$(TEST_MODULE).sv
 
+$(ROOT)/libs/googletest-release-1.10.0/Makefile: $(ROOT)/libs/googletest-release-1.10.0/CMakeLists.txt
+	cmake $(ROOT)/libs/googletest-release-1.10.0/Makefile
 
-$(TEST_OUTPUT): $(TEST_SOURCES) $(VERILATOR_MODULE_LIB)
-	g++ -DVL_THREADED -I$(ROOT)/test/common -I$(VERILATOR_INCLUDE) -I$(VERILATOR_GEN_DIR) -std=c++2a -fconcepts $(TEST_SOURCES) $(VERILATOR_INCLUDE)/verilated.cpp $(VERILATOR_INCLUDE)/verilated_threads.cpp $(VERILATOR_INCLUDE)/verilated_vcd_c.cpp -o $(TEST_OUTPUT)  $(VERILATOR_MODULE_LIB) -pthread
+$(ROOT)/libs/googletest-release-1.10.0/lib/libgtest_main.a: $(ROOT)/libs/googletest-release-1.10.0/Makefile
+	$(MAKE) -C $(ROOT)/libs/googletest-release-1.10.0/
+
+$(TEST_OUTPUT): $(TEST_SOURCES) $(VERILATOR_MODULE_LIB) $(ROOT)/libs/googletest-release-1.10.0/lib/libgtest_main.a
+	g++  -I$(ROOT)/libs/googletest-release-1.10.0/googletest/include -L$(ROOT)/libs/googletest-release-1.10.0/lib/ -I$(ROOT)/libs/googletest-release-1.10.0/ -DVL_THREADED -I$(ROOT)/test/common -I$(VERILATOR_INCLUDE) -I$(VERILATOR_GEN_DIR) -std=c++2a -fconcepts $(TEST_SOURCES) $(VERILATOR_INCLUDE)/verilated.cpp $(VERILATOR_INCLUDE)/verilated_threads.cpp $(VERILATOR_INCLUDE)/verilated_vcd_c.cpp -o $(TEST_OUTPUT)  $(VERILATOR_MODULE_LIB) -lgtest_main -lgtest -pthread
 
 clean:
 	rm -f $(TEST_OUTPUT)
 	rm -rf $(VERILATOR_GEN_DIR)
+	$(MAKE) -C $(ROOT)/libs/googletest-release-1.10.0/ clean
--- test/registerfile/main.cpp
+++ test/registerfile/main.cpp
@@ -2,9 +2,10 @@
 #include <verilated_vcd_c.h>
 #include <iostream>
 #include "Wrap.h"
+#include <gtest/gtest.h>
 
 
-int main(int argc, char const *argv[])
+int test(int argc, char const *argv[])
 {
     Verilated::traceEverOn(true);
     Vregisterfile rf;
@@ -12,22 +13,43 @@ int main(int argc, char const *argv[])
     Wrap<Vregisterfile> wrap ("test.vcd", &rf);
     rf.eval();
 
-    rf.RegisterFile__DOT__registers[2] = 13;
+    rf.RegisterFile__DOT__registers[29] = 13;
     rf.rst = 0;
     rf.en = 1;
     rf.we = 0;    
 
     for (int x = 0; x < 5; x++) {
         rf.clk = 1;
-        rf.reg_a = 1;
-        rf.reg_b = 2;
-        rf.reg_c = 1;
+        rf.reg_a = 30;
+        rf.reg_b = 29;
+        rf.reg_c = 30;
         rf.we = 1;
         rf.data_c = rf.data_a + rf.data_b;
         wrap.tick();
-        std::cout << "After " << x << " " << rf.RegisterFile__DOT__registers[1] << " " << rf.data_a << " " << rf.data_b << "\n";
+        std::cout << "After " << x + 1 << " " << rf.data_a << " " << rf.data_b << "\n";
     }
 
     /* code */
     return 0;
+}
+
+TEST(RegisterFileTest, X0NoWrite) {
+    Verilated::traceEverOn(true);
+    Vregisterfile rf;
+    Wrap<Vregisterfile> wrap ("X0NoWrite.vcd", &rf);
+    rf.eval();
+    rf.en = 1;
+    rf.reg_a = 0;
+    wrap.tick();
+    EXPECT_EQ(rf.data_a, 0);
+
+    rf.reg_c = 0;
+    rf.data_c = 10;
+    rf.we = 1;
+    wrap.tick();
+    EXPECT_EQ(rf.data_a, 0);
+
+    rf.we = 0;
+    wrap.tick();
+    EXPECT_EQ(rf.data_a, 0);
 }
